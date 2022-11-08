8dc93dbe69b94ce0c2d93164a314a52767163b98
Daniel Sweet <sweetpalma9@gmail.com>
2022-11-08 16:07:40 +0700

    added route

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 
@@ -34,8 +34,15 @@ async function run (){
             const result = await serviceCollection.find(query).toArray()
             res.send(result)
         })
+
+        app.get('/services/:id',async(req,res)=>{
+            const id = req.params.id
+            const query = {_id: ObjectId(id)}
+            const result = await serviceCollection.findOne(query)
+            res.send(result)
+        })
     }
-    catch{
+    finally{
 
     }
 }
