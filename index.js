0c3fffb0ea7842309d0b24c9dd7d656ecaa6abe5
Daniel Sweet <sweetpalma9@gmail.com>
2022-11-10 14:05:26 +0700

    vercel added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { query } = require("express");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 
@@ -26,40 +27,50 @@ const reviewCollection = client.db("electroService").collection("reviews");
 
 async function run() {
   try {
-
-
     //JWT
-    const verifyJwt = (req, res, next) =>{
-      const authHeader =  req.headers.authorization
-      if(!authHeader){
-       return res.status(401).send({message: 'unauthorized access'})
+    const verifyJwt = (req, res, next) => {
+      const authHeader = req.headers.authorization;
+      if (!authHeader) {
+        return res.status(401).send({ message: "unauthorized access" });
       }
-      const token = authHeader.split(' ')[1]
-      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded){
-          if(err){
-            res.send({message: 'unauthorized access'})
+      const token = authHeader.split(" ")[1];
+      jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET,
+        function (err, decoded) {
+          if (err) {
+            return res.send({ message: "unauthorized access" });
           }
-          req.decoded = decoded
-          next()
-      })
-    }
+          req.decoded = decoded;
+          next();
+        }
+      );
+    };
 
-    app.post('/jwt',(req,res)=>{
+    app.post("/jwt", (req, res) => {
       const user = req.body;
-      console.log(user)
-      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1h'})
-      res.send({token})
-    })
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
 
     //services
     app.get("/home/services", async (req, res) => {
       const query = {};
-      const result = await serviceCollection.find(query).limit(3).toArray();
+      const result = await serviceCollection
+        .find(query)
+        .sort({ $natural: -1 })
+        .limit(3)
+        .toArray();
       res.send(result);
     });
     app.get("/services", async (req, res) => {
       const query = {};
-      const result = await serviceCollection.find(query).toArray();
+      const result = await serviceCollection
+        .find(query)
+        .sort({ $natural: -1 })
+        .toArray();
       res.send(result);
     });
 
@@ -85,57 +96,92 @@ async function run() {
 
     //Reviews
 
-    //get reviews by id
-    app.get("/reviews/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { serviceId: id };
-      const result = await reviewCollection.find(query).toArray();
+   
+
+    
+
+    //get reviews by email
+    app.get("/reviews", verifyJwt, async (req, res) => {
+      const userEmail = req.query.email;
+
+      const decoded = req.decoded;
+      console.log(decoded);
+      if (decoded?.email !== userEmail) {
+        res.status(403).send({ message: "unauthorized acces" });
+      }
+      let query = {};
+      if (userEmail) {
+        query = {
+          email: userEmail,
+        };
+      }
+      const result = await reviewCollection
+        .find(query)
+        .sort({ time: -1 })
+        .toArray();
       res.send(result);
     });
 
-    //get reviews by email
-    app.get("/reviews",verifyJwt, async (req, res) => {
-      const userEmail = req.query.email
-
-      const decoded = req.decoded
-      console.log(decoded)
-      if(decoded.email !== userEmail){
-        res.status(403).send({message: 'unauthorized acces'})
-      }
+    app.get('/service/reviews',async(req,res)=>{
       let query = {}
-      if(userEmail){
-        query= {
-          email: userEmail
+      if(req.query.serviceId){
+        query = {
+          serviceId: req.query.serviceId
         }
       }
-      const result = await reviewCollection.find(query).toArray()
-      res.send(result)
-      
-    });
-    //post review
-    app.post("/reviews", async (req, res) => {
-      const { userName, userImage, review, serviceId, email, serviceName } = req.body;
-      const query = {
-        userName,
-        userImage,
-        review,
-        serviceId,
-        email,
-        serviceName
-      };
-      const result = await reviewCollection.insertOne(query);
+      const cursor = reviewCollection.find(query).sort({time: -1})
+      const reviews = await cursor.toArray()
+      res.send(reviews)
+    })
+
+     //post review
+     app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      review.time = Date();
+      const result = await reviewCollection.insertOne(review);
       res.send(result);
     });
 
     //delete review
-    app.delete('/reviews/:id', async(req,res)=>{
+    app.delete("/reviews/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: ObjectId(id)}
+      const query = { _id: ObjectId(id) };
 
-      const result = await reviewCollection.deleteOne(query)
-      res.send(result)
-    })
+      const result = await reviewCollection.deleteOne(query);
+      res.send(result);
+    });
 
+     //get reviews by id
+     app.get("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewCollection
+        .findOne(query)
+      res.send(result);
+    });
+
+   
+    //put review
+    app.put("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const filter = { _id: ObjectId(id) };
+      const review = req.body;
+      const option = { upsert: true };
+      const updateReview = {
+        $set: {
+          review: review.comment,
+        },
+      };
+      const result = await reviewCollection.updateOne(
+        filter,
+        updateReview,
+        option
+      );
+      res.send(result);
+    });
+
+    
   } finally {
   }
 }
